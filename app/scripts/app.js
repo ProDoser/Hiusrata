4b44a7b1d0688c6cd337ebaadaf18bd45015a38f
Leonid Zadorozhnykh <doser84@gmail.com>
2016-03-30 18:55:33 +0300

    New menu items. Mobile nav collapses on menu click

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -44,6 +44,9 @@ $(".nav a").on("click", function(){
     $(".nav").find(".active").removeClass("active");
     $(this).parent().addClass("active");
 });
+$(".navbar-brand").on("click", function(){
+    $(".nav").find(".active").removeClass("active");
+});
 
 
 
