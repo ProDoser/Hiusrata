1bfa6de9940a9d1d6c16c0cf1978cfd881f4181e
Leonid Zadorozhnykh <doser84@gmail.com>
2016-04-12 01:08:31 +0300

    Facebook page plugin added. Menu fixed to top

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -50,14 +50,3 @@ angular
         redirectTo: '/'
       });
   });
-
-/*$(".nav a").on("click", function(){
-    $(".nav").find(".active").removeClass("active");
-    $(this).parent().addClass("active");
-});
-$(".navbar-brand").on("click", function(){
-    $(".nav").find(".active").removeClass("active");
-});*/
-
-
-
